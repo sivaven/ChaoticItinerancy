ca11749b974670d7416bf0697c98b8ed42f06814
sivaven <sivaven7@gmail.com>
2018-05-07 13:10:39 -0400

    1

---
+++ clnup.sh
@@ -0,0 +1,5 @@
+git stash
+git pull
+
+rm nohup.out
+chmod u+rwx start_1.sh
--- start_1.sh
+++ start_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export set JAVA_OPTS="-XX:+AggressiveHeap"
-export CLASSPATH="src:bin:lib/*"
+export CLASSPATH="src:bin:lib/commons-math3-3.6/*"
 #find . -name "*.java" -print | xargs javac
 javac src/*.java 
 java NetworkStatesWrapper 7
